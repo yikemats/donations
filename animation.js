520828acfe1eaa63baafaa40b2c2913e266bd342
yikemats <74920547+yikemats@users.noreply.github.com>
2023-07-24 21:21:01 -0400

    Changes

---
+++ animation.js
@@ -0,0 +1,25 @@
+gsap.fromTo(
+    ".img1",
+    {
+      x: 500,
+    },
+    {
+      duration: 2,
+      x: 250,
+      rotate: 360,
+      repeat: 1,
+    }
+  );
+
+  // gsap.fromTo(
+  //   ".img2",
+  //   {
+  //     x: 10,
+  //   },
+  //   {
+  //     duration: 2,
+  //     x: 200,
+  //     rotate: 360,
+  //     repeat: 1,
+  //   }
+  // );
